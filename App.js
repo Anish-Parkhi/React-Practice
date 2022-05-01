142eb1252ecb7942c5d352688e74214779e1eefc
Anish-Parkhi <93964690+Anish-Parkhi@users.noreply.github.com>
2022-05-01 23:27:38 +0530

    Added project files

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import New from './componant/New';
+function App() {
+    return (
+      <div className="App">
+        <New />
+      </div>
+    );
+}
+
+export default App;
+
+
