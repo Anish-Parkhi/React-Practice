cb971a3f032251cf62436694aab0e7857f3751e7
Malhar Dixit <malharsoham@gmail.com>
2022-05-06 22:15:46 +0530

    UI changes

--- Desktop/react Gen-y/my-app/src/components/New.js
+++ Desktop/react Gen-y/my-app/src/components/New.js
@@ -1,22 +1,27 @@
-import react from 'react'
-import "./style.css"
-
+import react from "react";
+import "./style.css";
 
 const New = () => {
-   return (
-     <div className='parent'>
-         <h1 className='header'>inception 4.0</h1>
-         <p className='para'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-         <div>
-             {/*<h3 className='number'>4</h3> */} 
-              
-           {/* <h3 className='subheading'>Workshops</h3> */}
-         </div>
-         <div>
-         <h3 className='number1'>2000+</h3>
-         </div>
-         </div>
-   )
- }
+  return (
+    <div className="parent">
+      <h1 className="header">inception 4.0</h1>
+      <p className="para">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
 
- export default New;
+      <div>
+        <h3 className="number">4</h3>
+
+        {/* <h3 className='subheading'>Workshops</h3> */}
+      </div>
+      <div>
+        <h3 className="number1">2000+</h3>
+      </div>
+    </div>
+  );
+};
+
+export default New;
